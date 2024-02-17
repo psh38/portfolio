77987676dbd33d541affdadbd1fb965fd60e2ddf
psh38 <sosohy38@gmail.com>
2024-02-18 01:47:08 +0900

    검색어영역

--- js/common.js
+++ js/common.js
@@ -1,18 +1,43 @@
-// 헤더 전체메뉴 접기 펼침 기능
-document.querySelector('.btn-all-menu').addEventListener("click", (e) => {
-  console.log(e, 'all menu')
-  if(document.querySelector('body').classList.contains('all-menu') === false){
-   document.querySelector('body').classList.add('all-menu')   
+const body = document.querySelector('body');
+
+// 헤더 검색버튼 접기 펼침 기능
+const serchBtn = document.querySelector('header .gnb-wrap .btn-search');
+const searchCloseBtn = document.querySelector('header .gnb-wrap .btn-search-close');
+
+serchBtn.addEventListener('click', ()=>{
+  if(body.classList.contains('src-open') === false){
+    body.classList.add('src-open');
   }else{
-      document.querySelector('body').classList.remove('all-menu')
+    body.classList.remove('src-open');
+  }
+})
+
+searchCloseBtn.addEventListener('click', ()=>{
+  if(body.classList.contains('src-open') === false){
+    body.classList.add('src-open');
+  }else{
+    body.classList.remove('src-open');
+  }
+})
+
+
+
+// 헤더 전체메뉴 접기 펼침 기능
+const allMenuBtn = document.querySelector('header .gnb-wrap .btn-all-menu');
+const allColseBtn = document.querySelector('.sitemap-body .btn-all-close');
+
+allMenuBtn.addEventListener("click", () => {
+  if(body.classList.contains('all-menu') === false){
+   body.classList.add('all-menu')   
+  }else{
+      body.classList.remove('all-menu')
   }
 });
 
-document.querySelector('.btn-all-close').addEventListener("click", (e) => {
-  console.log(e, 'all menu')
-  if(document.querySelector('body').classList.contains('all-menu') === false){
-   document.querySelector('body').classList.add('all-menu')   
+allColseBtn.addEventListener("click", () => {
+  if(body.classList.contains('all-menu') === false){
+   body.classList.add('all-menu')   
   }else{
-      document.querySelector('body').classList.remove('all-menu')
+      body.classList.remove('all-menu')
   }
 });
