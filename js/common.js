2fdb385ec55fee0f52a3439c599d63407003f725
psh38 <sosohy38@gmail.com>
2024-02-24 00:18:47 +0900

    카드슬라이드 헤더 스크롤 커밋

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 // 카테고리 gnb
 const gnb = {
   openDelay: null, // gnb open 딜레이(접근성 고려)
-  gnbOpen: (() => {
+  gnbOpen: () => {
     document.querySelectorAll('header .menu-list > li > a').forEach((el) => {
       el.addEventListener('mouseover', (e) => {
         // e.preventDefault();
@@ -16,7 +16,7 @@ const gnb = {
               e.target.closest('.menu-list').querySelector('li.on').classList.remove('on')
             }
             e.target.closest('li').classList.add('on')
-            window.requestAnimationFrame(aniFrame.slideDown);
+            aniFrame.slideUpDown(true, e.target.closest('li').querySelector('.sub-area'));
           }, 200);
         }
       });
@@ -26,6 +26,7 @@ const gnb = {
             e.target.closest('.menu-list').querySelector('li.on').classList.remove('on')
           }
           e.target.closest('li').classList.add('on')
+          aniFrame.slideUpDown(true, e.target.closest('li').querySelector('.sub-area'));
         }
       });
 
@@ -37,81 +38,80 @@ const gnb = {
       });
     });
     gnb.gnbClose()
-  }),
-  gnbClose: (() => {
+  },
+  gnbClose: () => {
     document.querySelectorAll('header .menu-list > li').forEach((el) => {
       document.querySelector('header').addEventListener('mouseleave', (e) => {
         // console.log(el, 'gnb mouseleave')
-        el.classList.remove('on');
+        if(el.classList.contains('on')){
+          aniFrame.slideUpDown(false, el.querySelector('.sub-area'));
+        }
+        setTimeout(() => {
+          el.classList.remove('on');
+        }, 600);
       });
     });
-  })
+  },
+  sticky: () => {
+    if(document.querySelectorAll('html')[0].scrollTop > (window.outerHeight - document.querySelector("header").clientHeight)){ // 컨텐츠 두번째 영역부터 스티키
+      document.querySelector("header").classList.add("sticky");
+    }else{
+      document.querySelector("header").classList.remove("sticky");
+    };
+  }
 }
 
 // 공통 - 애니메이션 슬라이드 구현
 const aniFrame = {
-  speed: 50, // 애니메이션 속도
-  time: 300, // 애니메이션 시간 설정
-  aniVal: 0, // value
-  viewSet: false, // 열기, 닫기 기본 설정값
-  done: false, // 애니메이션 완료
-  start: undefined,
-  previousTimeStamp: false,
-  slideUp: (() => { // 슬라이드 닫기
+  slideShow: false, // 열기(true), 닫기(false)
+  slideId: null, // 애니메이션 함수 선언
+  slideHei: 0, // 높이값
+  slideMath: false, // 양수(true), 음수(false)
+  slideSpeed: 30, //  속도
+  slideMaxHei: 474, // 최대 높이값
+  element: null, // 선택자
+  slideUpDown: (event, target, speed, maxHei) => { // 슬라이드 열기, 닫기 (params: boolean(초기만 해당 두번째 실행부터는 time으로 리턴), target, speed, max-height)    
+    // console.log('slideUpDown', event, target)
+    // 초기 slideShow, slideMath 설정
+    if(typeof event === "boolean"){
+      aniFrame.element = target;
+
+
+      aniFrame.slideShow = event
+      aniFrame.slideMath = event
+
+      aniFrame.slideSpeed = maxHei != undefined ? speed : aniFrame.slideSpeed // 속도 재설정
+      aniFrame.slideMaxHei = maxHei != undefined ? maxHei : aniFrame.slideMaxHei // 최대 높이값 재설정
+    }
     
-  }),
-  slideDown: ((timestamp) => { // 슬라이드 열기
-    const element = document.querySelector(".sub-area");
-
-    // 맨 처음 시간 설정
-    if(aniFrame.start === undefined){
-      aniFrame.start = timestamp;
-    }
-    const elapsed = timestamp - aniFrame.start;
-    console.log('aaaaaaaa', timestamp, aniFrame.start, elapsed)
-
-    // 시간(time) 동안 애니메이션 진행
-    if(elapsed < aniFrame.time){
-      aniFrame.previousTimeStamp = timestamp;
-      if (!aniFrame.done) {
-        aniFrame.aniVal += aniFrame.speed
-        element.style.height = '474px';
-        window.requestAnimationFrame(aniFrame.slideDown);
-      }
+    // 애니메이션 열림(+), 닫힘(-) 체크
+    if(aniFrame.slideMath === true){
+      aniFrame.slideHei += aniFrame.slideSpeed // 양수
     }else{
-      aniFrame.done = true;
-      window.cancelAnimationFrame(aniFrame.slideDown);
+      aniFrame.slideHei -= aniFrame.slideSpeed // 음수
     }
+    // 애니메이션 반복 실행
+    cancelAnimationFrame(aniFrame.slideId)
+    aniFrame.slideId = requestAnimationFrame(aniFrame.slideUpDown)
+    aniFrame.element.style.maxHeight = `${aniFrame.slideHei}px`;
+    // console.log('애니메이션 프레임', aniFrame.slideHei)
 
-
-    // function step(timestamp) {
-    //   if (start === undefined) {
-    //     start = timestamp;
-    //   }
-    //   const elapsed = timestamp - start;
-    //   console.log('aaaaaaaa', timestamp, start, elapsed)
-
-    //   if (previousTimeStamp !== timestamp) {
-    //     // Math.min()은 여기서 요소가 정확히 200px에 멈추는지 확인하기 위해 사용됩니다
-    //     const count = Math.min(0.1 * elapsed, 200);
-    //     element.style.transform = `translateX(${count}px)`;
-    //     if (count === 200) done = true;
-    //   }
-
-    //   if (elapsed < 2000) {
-    //     // 2초 이후에 애니메이션 종료
-    //     previousTimeStamp = timestamp;
-    //     if (!done) {
-    //       window.requestAnimationFrame(step);
-    //     }
-    //   }
-    // }
-  })
+    // 애니메이션 정지
+    if(aniFrame.slideHei >= aniFrame.slideMaxHei && aniFrame.slideShow === true){ // 열림
+      cancelAnimationFrame(aniFrame.slideId)
+      aniFrame.slideMath = false
+      aniFrame.slideId = null
+    }else if(aniFrame.slideHei < 0){ // 닫힘
+      cancelAnimationFrame(aniFrame.slideId)
+      aniFrame.slideShow = false
+      aniFrame.slideId = null
+      aniFrame.slideHei = 0
+    }
+  }
 }
 
 // 함수 호츌
 gnb.gnbOpen() // 카테고리 gnb
-// aniFrame.slideDown() // 애니메이션 슬라이드
 
 
 
@@ -178,48 +178,68 @@ allColseBtn.addEventListener("click", () => {
 const scrollNaviList = document.querySelector('.scroll-navi .menu-list')
 const scrollNavis = scrollNaviList.querySelectorAll('li')
 const section = document.querySelectorAll('main > .section')
+let overlapMove = false // scroll-navi, scroll event 중복 방지
 
-
-for(let scrollNavi of scrollNavis){
-  scrollNavi.addEventListener('click', (e) => {
-    e.preventDefault();
-     // 클릭이벤트 앵커이동
-     let targetId = scrollNavi.querySelector('a').getAttribute('href');
-     // console.log(targetId);
-     let targetSection = document.querySelector(targetId);
-     let targetOST = targetSection.offsetTop;
-     // console.log(targetOST);
-     window.scrollTo({left:0, top:targetOST, behavior:'smooth'});
-
-    //  on클래스 / blind클래스 추가 제거
-    // console.log(scrollNavi.classList.contains('on'));
-    if(!scrollNavi.classList.contains('on')){
-      // console.log(scrollNavi.closest('.menu-list').querySelectorAll('li.on').length > 0);
-      if(scrollNaviList.querySelectorAll('li.on').length > 0){
-        scrollNaviList.querySelectorAll('li.on')[0].classList.remove('on');
-        scrollNavi.querySelector('span').classList.add('blind');
+// scroll move 관련 모듈
+const anchor = {
+  naviMove:() => { // scroll-navi 활성화
+    for (let idx = 0; idx < section.length; idx++) {
+      const contTop = section[idx].getBoundingClientRect().top;
+      const contHei = section[idx].clientHeight;
+      const headHei = parseInt(getComputedStyle(document.querySelector('header')).height); // 상단 헤더 높이값
+      
+      // console.log('스크롤 계산값', document.querySelectorAll('html')[0].scrollTop, section[idx].offsetTop)
+      // anchorMove 호출 -> 스크롤 완료 후 (유사 콜백)
+      if (section[idx].offsetTop === document.querySelectorAll('html')[0].scrollTop) {
+        overlapMove = false
       }
-      scrollNavi.classList.add('on');
-      scrollNavis.forEach((item) =>{
-         item.querySelector('a > span').classList.add('blind')
-      }) // 선택한 영역 외 blind 추가
-      if(scrollNavi.querySelector('.blind') != null){
-        scrollNavi.querySelector('.blind').classList.remove('blind');
+      if (contTop <= headHei && contTop + contHei >= headHei) {
+        if (!overlapMove) { // scroll-navi 클릭, scroll event 중복 방지
+          // 선택한 영역 외 on 제거
+          scrollNavis.forEach(item => {
+            item.classList.remove("on"); // :not 으로 사용 가능하다면 사용해도 무방
+            item.querySelector('a > span').classList.add('blind');
+          })
+          scrollNavis[idx].classList.add('on')
+          scrollNavis[idx].querySelector('a > span').classList.remove('blind')
+        }
       }
     }
-  });
-}
+  },
+  anchorMove:() => { // scroll-navi 클릭 시 앵커이동
+    for(let scrollNavi of scrollNavis){
+      scrollNavi.addEventListener('click', (e) => {
+        e.preventDefault();
+        overlapMove = true;
+         // 클릭이벤트 앵커이동
+         let targetId = scrollNavi.querySelector('a').getAttribute('href');
+         // console.log(targetId);
+         let targetSection = document.querySelector(targetId);
+         let targetOST = targetSection.offsetTop;
+         // console.log(targetOST);
+         window.scrollTo({left:0, top:targetOST, behavior:'smooth'});
+    
+        //  on클래스 / blind클래스 추가 제거
+        // console.log(scrollNavi.classList.contains('on'));
+        if(!scrollNavi.classList.contains('on')){
+          // console.log(scrollNavi.closest('.menu-list').querySelectorAll('li.on').length > 0);
+          if(scrollNaviList.querySelectorAll('li.on').length > 0){
+            scrollNaviList.querySelectorAll('li.on')[0].classList.remove('on');
+          }
+          scrollNavi.classList.add('on');
+    
+          console.log(scrollNavi.querySelector('.blind'));
+          scrollNavis.forEach(item => item.querySelector('a > span').classList.add("blind")) // 선택한 영역 외 blind 추가
+          if(scrollNavi.querySelector('.blind') != null){
+            scrollNavi.querySelector('.blind').classList.remove('blind');
+          }
+        }
+      });
+    };
+  },
+};
 
-// window.addEventListener('scroll',()=>{
-//   section.forEach((item,idx)=>{
-//     if(item.offsetTop <= window.scrollY){
-//       for(let navis of scrollNavis){
-//         navis.classList.remove('on');
-//       }
-//       navis[idx].classList.add('on');
-//     }
-//   })
-// });
+anchor.anchorMove() // scroll-navi 클릭 시 앵커이동
 
 // 푸터 패밀리 사이트
 const siteGroup = document.querySelector('.site-group');
@@ -233,6 +253,9 @@ siteGroup.addEventListener('click',() => {
 
 // 퀵버튼 위치조정
 window.addEventListener('scroll', () => {
+  gnb.sticky() // 헤더 스티키
+  anchor.naviMove() // 앵커이동
+
   let footerOffset = document.querySelector('footer').offsetTop;
   let quick = document.querySelector('.quick');
   if (window.scrollY + window.innerHeight > footerOffset) {
@@ -291,10 +314,6 @@ btnTop.addEventListener('click', () => {
   backToTop();
 })
 
-// btnTop.addEventListener('click', (e)=>{
-//   window.scrollTo({left:0, top:0, behavior:'smooth'});
-// });
-
 // 메인배너슬라이드
 const slideWrapper = document.querySelector('.slide-wrapper.main-bnr');
 const title = slideWrapper.querySelector('.card-info');
@@ -309,19 +328,15 @@ const slideToShow = 3;
 const prevBtn = slideWrapper.querySelector('.prev-btn');
 const nextBtn = slideWrapper.querySelector('.next-btn');
 
+// 로드 시 카드이벤트 정보 추가
+let content = cardInfo[currentIdx+1].innerHTML;
+console.log(content)
+title.innerHTML= content;
+
 //슬라이드 배치, slideContainer의 너비를 지정
 slideContainer.style.width = slideWidth*slideCount + slideMargin*(slideCount-1)+'px';
 
-//이동함수
-/*
-moveSlide함수는 매개변수 idx가 들어오면 할일
-  idx숫자를 활용해서 slideContainer의 translateX값을 변경한다.
-  .slideContainer {
-    width: 100px;
-    transform:translate(-230px);
-  }
-  slideContainer.style.width = 100px;
-*/
+// 이동함수
 function moveSlide(idx){
   slideContainer.style.transform = `translateX(${-idx*(slideWidth + slideMargin)}px)`;
   currentIdx = idx;
@@ -330,7 +345,7 @@ function moveSlide(idx){
     slide.classList.remove('active');
   }
   slides[idx+1].classList.add('active');
-  let content = cardInfo[idx+1].innerHTML;
+  content = cardInfo[idx+1].innerHTML;
   title.innerHTML= content;
 }
 //다음 버튼으로 이동하기
